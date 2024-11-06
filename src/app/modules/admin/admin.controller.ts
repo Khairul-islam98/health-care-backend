f5d18429cd093f9bb10276d6ba9f570409b62e37
khairul-islam98 <janeyrahman900@gmail.com>
2024-11-06 08:03:49 +0600

    implement admin retrieval functionality

--- src/app/modules/admin/admin.controller.ts
+++ src/app/modules/admin/admin.controller.ts
@@ -1,7 +1,14 @@
 import { Request, Response } from "express";
+import { adminService } from "./admin.service";
 
 const getAllFromDb = async (req: Request, res: Response) => {
   try {
+    const result = await adminService.getAllFromDB();
+    res.status(200).json({
+      success: true,
+      message: "Successfully fetched all admins",
+      data: result,
+    });
   } catch (error) {
     console.log(error);
     res.status(500).json({
