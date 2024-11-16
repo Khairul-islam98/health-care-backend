15244fd557e6c8c5678a6b9ebb84ef2c8cdf96f7
khairul-islam98 <janeyrahman900@gmail.com>
2024-11-16 15:24:36 +0600

    add user profile retrieval and update routes to UserRoutes

--- src/app/modules/users/user.route.ts
+++ src/app/modules/users/user.route.ts
@@ -9,7 +9,7 @@ const router = express.Router();
 router.get(
   "/",
   auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
-  UserController.getAllUsers
+  UserController.getAllUsers,
 );
 
 router.post(
@@ -19,7 +19,7 @@ router.post(
   (req: Request, res: Response, next: NextFunction) => {
     req.body = JSON.parse(req.body.data);
     return UserController.createAdmin(req, res);
-  }
+  },
 );
 
 router.post(
@@ -29,7 +29,7 @@ router.post(
   (req: Request, res: Response, next: NextFunction) => {
     req.body = JSON.parse(req.body.data);
     return UserController.createDoctor(req, res, next);
-  }
+  },
 );
 
 router.post(
@@ -38,13 +38,29 @@ router.post(
   (req: Request, res: Response, next: NextFunction) => {
     req.body = JSON.parse(req.body.data);
     return UserController.createPatient(req, res, next);
-  }
+  },
 );
 
 router.patch(
   "/:id/status",
   auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
-  UserController.changeProfileStatus
+  UserController.changeProfileStatus,
+);
+
+router.get(
+  "/me",
+  auth(UserRole.SUPER_ADMIN, UserRole.DOCTOR, UserRole.ADMIN, UserRole.PATIENT),
+  UserController.getMyProfile,
+);
+
+router.patch(
+  "/update-my-profile",
+  auth(UserRole.SUPER_ADMIN, UserRole.DOCTOR, UserRole.ADMIN, UserRole.PATIENT),
+  fileUpload.upload.single("file"),
+  (req: Request, res: Response, next: NextFunction) => {
+    req.body = JSON.parse(req.body.data);
+    return UserController.updateMyProfile(req, res, next);
+  },
 );
 
 export const UserRoutes = router;
