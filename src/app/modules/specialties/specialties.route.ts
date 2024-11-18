2f05404053cb6f073f32d4141e742a48afae20a6
khairul-islam98 <janeyrahman900@gmail.com>
2024-11-18 19:52:46 +0600

    add routes for retrieving, inserting, and deleting specialties with file upload support

--- src/app/modules/specialties/specialties.route.ts
+++ src/app/modules/specialties/specialties.route.ts
@@ -1,5 +1,24 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import { SpecialtiesControllers } from "./specialties.controller";
+import { fileUpload } from "../../helpars/fileUploader";
+import auth from "../../middlewares/auth";
+import { UserRole } from "@prisma/client";
 
 const router = express.Router();
+
+router.get("/", SpecialtiesControllers.getAllSpecialties);
+router.post(
+  "/",
+  fileUpload.upload.single("file"),
+  (req: Request, res: Response, next: NextFunction) => {
+    req.body = JSON.parse(req.body.data);
+    return SpecialtiesControllers.insertInto(req, res, next);
+  }
+);
+router.delete(
+  "/:id",
+  auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
+  SpecialtiesControllers.deleteSpecialties
+);
 
 export const SpecialtiesRoutes = router;
