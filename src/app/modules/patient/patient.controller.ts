1a7c853e28d1a62dfe10869f30f3c8652951f726
khairul-islam98 <janeyrahman900@gmail.com>
2024-11-20 10:52:33 +0600

    add getAllPatient controller and route for fetching all patients

--- src/app/modules/patient/patient.controller.ts
+++ src/app/modules/patient/patient.controller.ts
@@ -1,3 +1,22 @@
+import pick from "../../../shared/pick";
+import catchAsync from "../../utils/catchAsync";
+import sendResponse from "../../utils/sendResponse";
+import { patientFilterableFields } from "./patient.constant";
+import { PatientServices } from "./patient.service";
+import httpStatus from "http-status";
 
+const getAllPatient = catchAsync(async (req, res) => {
+  const filter = pick(req.query, patientFilterableFields);
+  const option = pick(req.query, ["limit", "page", "sortBy", "sortOrder"]);
+  const result = await PatientServices.getAllPatientIntoDB(filter, option);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "All patients fetched successfully",
+    data: result,
+  });
+});
 
-
+export const PatientControllers = {
+  getAllPatient,
+};
--- src/app/modules/patient/patient.route.ts
+++ src/app/modules/patient/patient.route.ts
@@ -1,5 +1,8 @@
 import express from "express";
+import { PatientControllers } from "./patient.controller";
 
 const router = express.Router();
+
+router.get("/", PatientControllers.getAllPatient);
 
 export const PatientRoutes = router;
