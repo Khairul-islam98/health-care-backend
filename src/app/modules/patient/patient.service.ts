6593e1e716d5d741c66977bac8b08063f3de6cc0
khairul-islam98 <janeyrahman900@gmail.com>
2024-11-20 10:48:31 +0600

    refactor patient module: rename MedicalRepost to MedicalReport, implement patient search and pagination service

---
+++ src/app/modules/patient/patient.service.ts
@@ -0,0 +1,65 @@
+import { Patient, Prisma } from "@prisma/client";
+import { paginationHelper } from "../../helpars/paginationHelper";
+import { patientSearchableFields } from "./patient.constant";
+import prisma from "../../../shared/prisma";
+
+const getAllPatientIntoDB = async (params: any, option: any) => {
+  const { limit, page, skip } = paginationHelper.calculatePagination(option);
+  const { searchTerm, ...filterData } = params;
+
+  const andCondition: Prisma.PatientWhereInput[] = [];
+
+  if (params.searchTerm) {
+    andCondition.push({
+      OR: patientSearchableFields.map((field) => ({
+        [field]: {
+          contains: params.searchTerm,
+          mode: "insensitive",
+        },
+      })),
+    });
+  }
+  if (Object.keys(filterData).length > 0) {
+    andCondition.push({
+      AND: Object.keys(filterData).map((key) => ({
+        [key]: {
+          equals: filterData[key],
+          mode: "insensitive",
+        },
+      })),
+    });
+  }
+  andCondition.push({
+    isDeleted: false,
+  });
+  const whereCondition: Prisma.PatientWhereInput =
+    andCondition.length > 0 ? { AND: andCondition } : {};
+  const result = await prisma.patient.findMany({
+    where: whereCondition,
+    skip,
+    take: limit,
+    orderBy:
+      option.sortBy && option.sortOrder
+        ? { [option.sortBy]: option.sortOrder }
+        : { createdAt: "desc" },
+    include: {
+      medicalReport: true,
+      patientHealthData: true,
+    },
+  });
+  const total = await prisma.patient.count({
+    where: whereCondition,
+  });
+  return {
+    meta: {
+      page,
+      limit,
+      total,
+    },
+    data: result,
+  };
+};
+
+export const PatientServices = {
+  getAllPatientIntoDB,
+};
