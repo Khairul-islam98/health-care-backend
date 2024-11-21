43b906923c747656e5a93acd397c445df9b4ac7a
khairul-islam98 <janeyrahman900@gmail.com>
2024-11-21 14:10:37 +0600

    function to handle scheduling with interval checks

--- src/app/modules/schedule/schedule.servie.ts
+++ src/app/modules/schedule/schedule.servie.ts
@@ -1,7 +1,12 @@
-import { addHours, format } from "date-fns";
+import { addHours, addMinutes, format } from "date-fns";
+import prisma from "../../../shared/prisma";
 
 const createScheduleIntoDB = async (payload: any) => {
   const { startDate, endDate, startTime, endTime } = payload;
+
+  const interverlTime = 30;
+
+  const schedules = [];
 
   const currentDate = new Date(startDate);
   const lastDate = new Date(endDate);
@@ -10,19 +15,37 @@ const createScheduleIntoDB = async (payload: any) => {
     const startDateTime = new Date(
       addHours(
         `${format(currentDate, "yyyy-MM-dd")}`,
-        Number(startTime.split(":")[0])
-      )
+        Number(startTime.split(":")[0]),
+      ),
     );
     const endDateTime = new Date(
       addHours(
         `${format(lastDate, "yyyy-MM-dd")}`,
-        Number(endTime.split(":")[0])
-      )
+        Number(endTime.split(":")[0]),
+      ),
     );
     while (startDateTime <= endDateTime) {
-      //TODO: next add
+      const scheduleData = {
+        startDateTime: startDateTime,
+        endDateTime: addMinutes(startDateTime, interverlTime),
+      };
+      const existingSchedule = await prisma.schedule.findFirst({
+        where: {
+          startDateTime: scheduleData.startDateTime,
+          endDateTime: scheduleData.endDateTime,
+        },
+      });
+      if (!existingSchedule) {
+        const result = await prisma.schedule.create({
+          data: scheduleData,
+        });
+        schedules.push(result);
+      }
+      startDateTime.setMinutes(startDateTime.getMinutes() + interverlTime);
     }
+    currentDate.setDate(currentDate.getDate() + 1);
   }
+  return schedules;
 };
 
 export const ScheduleServices = {
